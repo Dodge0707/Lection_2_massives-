bc7ec413a1ce298c94b668d778e41f1dd325dbd9
«Vova <vrobskiy@gmail.com>
2022-04-13 19:16:05 +0300

    add Array Find

---
+++ Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 1, 12, 3, 45, 643, 12, 6453, 123};
+
+int n = array.Length;
+int find = 12; 
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
